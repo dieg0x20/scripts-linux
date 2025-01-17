2193f413414c422049e31dcb203a1c95042140d5
Diego <diego444x@gmail.com>
2025-01-16 21:18:05 -0300

    baner

--- secure_shell.sh
+++ secure_shell.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 #baner
+clear
+
+echo -e "\033[1m\033[33m
 
 
+██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+███████║███████║██║     █████╔╝ █████╗  ██████╔╝
+██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+
+"
 MAKER_FILE="$HOME/.password_checked"
 PASSWORD="eliot"
 
-if [ -f "$MAKER_FILE"]; then
+if [ -f "$MAKER_FILE" ]; then
 	exit 0
 fi
 
@@ -15,14 +26,12 @@ read -s input_password
 echo
 
 if [ "$input_password" == "$PASSWORD" ]; then
-	echo "Senha correta. Acesso liberado"
 	touch "$MAKER_FILE"
+	clear
 	bash
 else
-	echo "Senha incorreta. Sistema travado"
-	cmatrix &
-	trap '' 2 20
-	while true; do
-		sleep 1
-	done
+	echo ""
+	echo -e "\e[1;31m  You Entered wrong Details!\e[0m"
+	sleep 1
+	cmatrix -L
 fi
