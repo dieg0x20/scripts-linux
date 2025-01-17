2e4f07a8d236d71f2c02e1d0758e0b3203a149a3
Diego <diego444x@gmail.com>
2025-01-16 21:29:15 -0300

    desabilitar comandos do bash

--- secure_shell.sh
+++ secure_shell.sh
@@ -11,7 +11,7 @@ echo -e "\033[1m\033[33m
 ███████║███████║██║     █████╔╝ █████╗  ██████╔╝
 ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
 ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
-╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                    
 
 "
 MAKER_FILE="$HOME/.password_checked"
@@ -20,6 +20,15 @@ PASSWORD="eliot"
 if [ -f "$MAKER_FILE" ]; then
 	exit 0
 fi
+
+
+disable_commands() {
+    trap '' 2  # Ignora o Ctrl+C (SIGINT)
+    trap '' 3  # Ignora o Ctrl+\ (SIGQUIT)
+    trap '' 15 # Ignora o sinal de término (SIGTERM)
+}
+
+disable_commands
 
 echo -n "Digite a senha de acesso: "
 read -s input_password
